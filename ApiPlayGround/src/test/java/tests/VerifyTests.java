beb701b2b7b7e02dba250605fc12dd5aada756a6
kamel <kamel@DESKTOP-ID5FVM1>
2019-08-24 12:08:25 +0200

    Done a little modification on the verify test to make sure the request_id returned in not null

--- ApiPlayGround/src/test/java/tests/VerifyTests.java
+++ ApiPlayGround/src/test/java/tests/VerifyTests.java
@@ -3,7 +3,7 @@ package tests;
 import io.restassured.http.ContentType;
 import utils.VerifyPayLoad;
 import static io.restassured.RestAssured.given;
-
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 public class VerifyTests extends Tests{
@@ -18,13 +18,17 @@ public class VerifyTests extends Tests{
 		payload.brand = "NexmoVerifyTest";
 		payload.number = PHONE_NUMBER;
 		
-		 given()
+		String request_id = given()
 			.contentType(ContentType.JSON)
 			.body(payload)
 			.when()
 			.post("/verify/json")
 			.then()
-			.statusCode(200);	
+			.statusCode(200)
+			.extract()
+			.path("request_id");
+		
+		assertNotNull(request_id);
 		
 	}
 
